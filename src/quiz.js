0d9f8ac09dad507e35e004694fb6f369100074d5
Geethu Pandath Ramesh <geethuramesh2@gmail.com>
2024-10-07 15:27:13 +0200

    miniproject-day1 done

--- src/quiz.js
+++ src/quiz.js
@@ -12,4 +12,35 @@ class Quiz {
     // 5. checkAnswer(answer)
 
     // 6. hasEnded()
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex =0;
+    }
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex];
+    }  
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
+    shuffleQuestions(){
+        for(let i=this.questions.length; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+    }
+    checkAnswer(answer){
+        const currentQuestion=this.getQuestion();
+
+        if(currentQuestion.answer === answer){
+            this.correctAnswers++;
+        }
+
+    }
+    hasEnded(){
+        return this.currentQuestionIndex >= this.questions.length;
+
+        }
 }
