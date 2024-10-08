2684503cd3bcc9db7bc1619a83bd59fdda227e05
Geethu Pandath Ramesh <geethuramesh2@gmail.com>
2024-10-08 15:37:37 +0200

    miniproject-day 2

--- src/quiz.js
+++ src/quiz.js
@@ -4,7 +4,7 @@ class Quiz {
     // 1. constructor (questions, timeLimit, timeRemaining)
 
     // 2. getQuestion()
-    
+
     // 3. moveToNextQuestion()
 
     // 4. shuffleQuestions()
@@ -12,35 +12,50 @@ class Quiz {
     // 5. checkAnswer(answer)
 
     // 6. hasEnded()
-    constructor (questions, timeLimit, timeRemaining) {
+    constructor(questions, timeLimit, timeRemaining) {
         this.questions = questions;
         this.timeLimit = timeLimit;
         this.timeRemaining = timeRemaining;
         this.correctAnswers = 0;
-        this.currentQuestionIndex =0;
+        this.currentQuestionIndex = 0;
     }
-    getQuestion(){
+    getQuestion() {
         return this.questions[this.currentQuestionIndex];
-    }  
+    }
     moveToNextQuestion() {
         this.currentQuestionIndex++;
     }
-    shuffleQuestions(){
-        for(let i=this.questions.length; i > 0; i--) {
+    shuffleQuestions() {
+        for (let i = this.questions.length; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
         }
     }
-    checkAnswer(answer){
-        const currentQuestion=this.getQuestion();
+    checkAnswer(answer) {
+        const currentQuestion = this.getQuestion();
 
-        if(currentQuestion.answer === answer){
+        if (currentQuestion.answer === answer) {
             this.correctAnswers++;
         }
 
     }
-    hasEnded(){
+    hasEnded() {
         return this.currentQuestionIndex >= this.questions.length;
 
+    }
+    filterQuestionsByDifficulty(difficulty) {
+        if (typeof (difficulty) === 'number' && difficulty >= 1 && difficulty <= 3) {
+            this.questions = this.questions.filter(question => question.difficulty === difficulty)
+        //return this.questions;
         }
+
+
+    }
+    averageDifficulty(){
+        const sumofDifficulty = this.questions.reduce((sum,currentValue) => sum + currentValue.difficulty, 0);
+        return sumofDifficulty/this.questions.length;
+    }
+
 }
+    
+
